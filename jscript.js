cd0a6141ad88a3c6b90343894b4ff0311602528c
NickKumar <Timerbob@gmail.com>
2022-07-31 13:42:45 -0600

    Version 2

--- jscript.js
+++ jscript.js
@@ -11,13 +11,17 @@ const random25 = function () {
   console.log(random);
 };
 
-const gameover = function () {
-  if (chances === 0) {
-    console.log("gameover");
-    gameoverScreen.classList.add("showLose");
-    textbox.textContent = `Sorry!`;
-  }
-};
+// const gameover = function () {
+//   if (chances === 0) {
+//     console.log("gameover");
+
+//     // gameoverScreen.classList.add("showLose");
+//     // textbox.textContent = `Sorry!`;
+//     // setTimeout(function () {
+//     //   gameoverScreen.classList.add("opacity");
+//     // }, 5000);
+//   }
+// };
 
 const reset = function () {
   for (let i = 0; i < 25; i++) {
@@ -28,11 +32,6 @@ const reset = function () {
     gamebox.append(newCard);
   }
   const cardContainer = document.querySelectorAll(".card-container");
-
-  //   const inner = `<div class="card-inner">${index}
-  // <div class="card-front"></div>
-  // <div class="card-back"></div>
-  // </div>`;
 
   cardContainer.forEach(function (card, index) {
     card.innerHTML = `<div class="card-inner">
@@ -55,23 +54,53 @@ reset();
 gamebox.addEventListener("click", function (e) {
   const clicked = e.target.closest(".card-inner");
   if (!clicked) return;
-  clicked.classList.add("flip-card");
-  console.log(clicked.closest(".card-container").getAttribute("data-number"));
-  if (
-    random ===
-    Number(clicked.closest(".card-container").getAttribute("data-number"))
-  ) {
-    clicked.querySelector(".card-back").style.backgroundColor = "#8ac926";
-    console.log("You win!");
-    gameoverScreen.classList.add("showLose");
-    textbox.textContent = `Winner!!!`;
-  }
-  chances--;
-  gameover();
-});
 
-tryAgain.addEventListener("click", function () {
-  gamebox.textContent = "";
-  reset();
-  gameoverScreen.classList.remove("showLose");
+  const numberClicked = Number(
+    clicked.closest(".card-container").getAttribute("data-number")
+  );
+
+  if (chances > 0) {
+    clicked.classList.add("flip-card");
+  }
+  console.log(numberClicked);
+  chances--;
+  if (random === numberClicked) {
+    const cardBack = clicked.querySelector(".card-back");
+    cardBack.style.background = `linear-gradient(135deg, rgba(90,218,158,1) 33%, rgba(169,237,131,1) 66%)`;
+    cardBack.classList.add("textbox");
+    console.log("You win!");
+    chances = 0;
+    cardBack.textContent = `Winner!!`;
+    cardBack.classList.add("blinking");
+    setTimeout(function () {
+      cardBack.textContent = "Play Again?";
+      cardBack.classList.remove("blinking");
+      cardBack.addEventListener("click", function () {
+        gamebox.textContent = "";
+        reset();
+      });
+    }, 2000);
+  }
+
+  if (random !== numberClicked && chances === 0) {
+    const cardBack = clicked.querySelector(".card-back");
+    cardBack.style.background = `linear-gradient(
+      135deg,
+      rgba(158, 86, 152, 1) 33%,
+      rgba(175, 70, 121, 1) 66%
+    )`;
+    cardBack.classList.add("textbox");
+    console.log("Sorry!");
+    chances = 0;
+    cardBack.textContent = `Sorry!!`;
+    cardBack.classList.add("blinking");
+    setTimeout(function () {
+      cardBack.textContent = "Play Again?";
+      cardBack.classList.remove("blinking");
+      cardBack.addEventListener("click", function () {
+        gamebox.textContent = "";
+        reset();
+      });
+    }, 2000);
+  }
 });
