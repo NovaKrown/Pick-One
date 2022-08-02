3a15144af6cffb56769a02975b179f9d49f07c59
NickKumar <Timerbob@gmail.com>
2022-08-02 12:26:39 -0600

    Version 4

--- jscript.js
+++ jscript.js
@@ -14,17 +14,9 @@ const random25 = function () {
   console.log(random);
 };
 
-// const gameover = function () {
-//   if (chances === 0) {
-//     console.log("gameover");
-
-//     // gameoverScreen.classList.add("showLose");
-//     // textbox.textContent = `Sorry!`;
-//     // setTimeout(function () {
-//     //   gameoverScreen.classList.add("opacity");
-//     // }, 5000);
-//   }
-// };
+const refreshCounter = function () {
+  counterText.innerHTML = `Chances left : ${chances}`;
+};
 
 const reset = function () {
   for (let i = 0; i < 25; i++) {
@@ -46,38 +38,23 @@ const reset = function () {
       setTimeout(function () {
         card.classList.add("show");
       }, 150 * index);
+      chances = 5;
+      refreshCounter();
     }, 500);
   });
   random25();
-  chances = 6;
-  counterText.innerHTML = `Chances left: ${chances - 1}`;
 };
 
 reset();
-chances--;
 
-gamebox.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".card-inner");
-  const numberClicked = Number(
-    clicked.closest(".card-container").getAttribute("data-number")
-  );
-
-  if (!clicked) return;
-
-  if (chances > 0) {
-    clicked.classList.add("flip-card");
-  }
-  console.log(numberClicked);
-
+const winner = function (numberClicked, clicked) {
   if (random === numberClicked) {
     const cardBack = clicked.querySelector(".card-back");
     cardBack.style.background = `linear-gradient(135deg, rgba(90,218,158,1) 33%, rgba(169,237,131,1) 66%)`;
     cardBack.classList.add("textbox");
     console.log("You win!");
-    chances = 0;
     cardBack.textContent = `Winner!!`;
     cardBack.classList.add("blinking");
-    counterText.innerHTML = `Chances left:`;
     setTimeout(function () {
       cardBack.textContent = "Play Again?";
       cardBack.classList.remove("blinking");
@@ -86,35 +63,48 @@ gamebox.addEventListener("click", function (e) {
         reset();
       });
     }, 2000);
+    chances = 0;
   }
+};
 
-  if (random !== numberClicked) {
-    chances--;
-    console.log(`chances${chances}`);
+gamebox.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".card-inner");
+  if (!clicked) return;
+  const numberClicked = Number(
+    clicked.closest(".card-container").getAttribute("data-number")
+  );
+
+  if (chances > 0) {
+    clicked.classList.add("flip-card");
+  }
+  console.log(numberClicked);
+
+  winner(numberClicked, clicked);
+
+  if (random !== numberClicked && chances !== 0) {
     const cardBack = clicked.querySelector(".card-back");
+    if (chances > 0) {
+      chances = chances - 1;
+    }
     cardBack.style.background = `linear-gradient(
       135deg,
       rgba(158, 86, 152, 1) 33%,
       rgba(175, 70, 121, 1) 66%
     )`;
-    cardBack.classList.add("textbox");
     if (chances === 0) {
+      cardBack.classList.add("textbox");
       console.log("Sorry!");
       cardBack.textContent = `Sorry!!`;
       cardBack.classList.add("blinking");
-      counterText.innerHTML = `Chances left: 0`;
       setTimeout(function () {
         cardBack.textContent = "Play Again?";
         cardBack.classList.remove("blinking");
         cardBack.addEventListener("click", function () {
           gamebox.textContent = "";
-          chances = 0;
           reset();
         });
       }, 2000);
     }
   }
-  if (chances > 0 && chances < 5) {
-    counterText.innerHTML = `Chances left: ${chances}`;
-  }
+  refreshCounter();
 });
